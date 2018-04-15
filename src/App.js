a765c94bd165a7f941f1a98256b16a61d9756d06
ToddWebDev <>
2018-04-15 14:07:00 -0500

    Add state using a Class Component instead of a Function Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const handle = '@toddwebdev'
-const nav = ['All','Javascript','Ruby', 'Python']
-
 function Nav (props) {
   return (
     <ul>
@@ -20,17 +17,24 @@ function HandleComponent (props) {
   return <p>{props.handle}</p>
 }
 
-class App extends Component {  
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nav: ['All','Javascript','Ruby', 'Python'],
+      handle: '@toddwebdev'
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="jumbotron">
           <h1 className="display-4">Hello, from the React World!</h1>
           <p className="lead">To get started, caffiene up mate <code>src/App.js</code> and save to reload.</p>
-          <HandleComponent handle={handle} />
+          <HandleComponent handle={this.state.handle} />
           <a className="btn btn-primary btn-lg" href="" role="button">Learn more</a>
         </div>
-        <Nav list={nav} />
+        <Nav list={this.state.nav} />
       </div>
     );
   }
