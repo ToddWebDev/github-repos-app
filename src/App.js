4cb0fec0035be8869c6c6608c05fcdfb8dc147ad
ToddWebDev <>
2018-04-15 13:58:48 -0500

    Add stateless Nav component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const name = 'ToddWebDev'
 const handle = '@toddwebdev'
+const nav = ['All','Javascript','Ruby', 'Python']
 
-function NameComponent (props) {
-  return <h4>{props.name}</h4>
+function Nav (props) {
+  return (
+    <ul>
+      {props.list.map((name) => (
+        <li key={name}>
+          {name}
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 function HandleComponent (props) {
-  return <h4>{props.handle}</h4>
+  return <p>{props.handle}</p>
 }
 
-class App extends Component {
+class App extends Component {  
   render() {
     return (
       <div className="App">
         <div className="jumbotron">
-          <h1 className="display-4">Hello, from the {name}!</h1>
+          <h1 className="display-4">Hello, from the React World!</h1>
           <p className="lead">To get started, caffiene up mate <code>src/App.js</code> and save to reload.</p>
+          <HandleComponent handle={handle} />
           <a className="btn btn-primary btn-lg" href="" role="button">Learn more</a>
         </div>
-        <NameComponent name={name} />
-        <HandleComponent handle={handle} />
+        <Nav list={nav} />
       </div>
     );
   }
