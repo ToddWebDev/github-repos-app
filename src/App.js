103177c87d5cd66625618781bfeb567dc273ca16
ToddWebDev <>
2018-04-15 12:31:32 -0500

    My first stateless functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const name = 'ToddWebDev'
+const handle = '@toddwebdev'
+
+function NameComponent (props) {
+  return <h4>{props.name}</h4>
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="jumbotron">
-          <h1 className="display-4">Hello, from the React World!</h1>
+        <div className="jumbotron">
+          <h1 className="display-4">Hello, from the {name}!</h1>
           <p className="lead">To get started, caffiene up mate <code>src/App.js</code> and save to reload.</p>
           <a className="btn btn-primary btn-lg" href="" role="button">Learn more</a>
         </div>
+        <NameComponent name={name} />
       </div>
     );
   }
