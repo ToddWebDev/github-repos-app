b46e5b0d8e1ae036c407d8c0beac9918221f2dfd
ToddWebDev <>
2018-04-22 10:06:49 -0500

    Adding Loading Class Component

--- src/App.js
+++ src/App.js
@@ -31,13 +31,42 @@ function HandleComponent (props) {
   return <p>{props.handle}</p>
 }
 
+//Class Components with State and render
+class Loading extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Loading'
+    };
+  }
+  componentDidMount() {
+    const stopper = this.state.text + '...';
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({ text: 'Loading' }))
+        : this.setState((prevState) => ({ text: prevState.text + '.' }))
+    }, 300)
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       repos: [],
       handle: '@toddwebdev',
-      activeLanguage: 'All'
+      activeLanguage: 'All',
+      loading: true
     }
     
     this.handleSelectLanguage = this.handleSelectLanguage.bind(this)
@@ -92,7 +121,7 @@ class App extends Component {
   handleRemoveRepo(name) {
     this.setState((currentState) => {
       return {
-        repos: currentState.repos.filter((repo) => repo !== name)
+        repos: currentState.repos.filter((repo) => repo.name !== name)
       }
     })
   }
@@ -124,13 +153,14 @@ class App extends Component {
           <hr/>
           <div className="row">
             <div className="col-sm">
-            <h1 style={{textAlign: 'center'}}>
-              {this.state.activeLanguage}
-            </h1>
-              <ReposList
-                repos={this.state.repos}
-                onRemoveRepo={this.handleRemoveRepo}
-              />
+            {this.state.loading === true
+              ? <Loading />
+              : <div>
+                  <h1 style={{textAlign: 'center'}}>
+                    {this.state.activeLanguage}
+                  </h1>
+                  <ReposList repos={this.state.repos} onRemoveRepo={this.handleRemoveRepo} />
+                </div>}
             </div>
           </div>
         </div>
