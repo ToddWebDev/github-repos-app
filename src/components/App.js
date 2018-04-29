435913a14b0a367e2f117d29580e1c2ca11f4bc0
ToddWebDev <webdevtodd@gmail.com>
2018-04-29 17:29:14 -0500

    Remove unused Handlers in App.js and add Results Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ var Nav = require('./Nav')
 var Home = require('./Home')
 var Battle = require('./Battle')
 var Popular = require('./Popular')
+var Results = require('./Results')
 var ReactRouter = require('react-router-dom')
 var Router = ReactRouter.BrowserRouter
 var Route = ReactRouter.Route
@@ -11,10 +12,6 @@ var Switch = ReactRouter.Switch
 
 
 //Stateless Functional Components
-function HandleComponent (props) {
-  return <p>{props.handle}</p>
-}
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -23,70 +20,21 @@ class App extends Component {
       handle: '@toddwebdev',
       activeLanguage: 'All',
       loading: true
-    }
-    
-    this.handleSelectLanguage = this.handleSelectLanguage.bind(this)
-    this.fetchRepos = this.fetchRepos.bind(this)
-    this.handleRemoveRepo = this.handleRemoveRepo.bind(this)
-    
+    }   
     console.log('--constructor--')
   }
   
   //Component Lifecycle hooks
   componentDidMount() {
     console.log('--componentDidMount--')
-    window.API = {
-      fetchPopularRepos(language = 'all') {
-        const encodedURI = encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
-        
-        return fetch(encodedURI)
-          .then((data) => data.json())
-          .then((repos) => repos.items)
-          .catch((error) => {
-            console.warn(error)
-            return null
-          });
-      }
-    }
-    this.fetchRepos(this.state.activeLanguage)
   }
   componentDidUpdate(prevProps, prevState) {
     console.log('--componentDidUpdate--')
-    if (prevState.activeLanguage !== this.state.activeLanguage) {
-      this.fetchRepos(this.state.activeLanguage)
-    }
   }
   componentWillUnmount() {
     console.log('--componentWillUnmount--')
   }
-  
-  fetchRepos(lang) {
-    this.setState({
-      loading: true
-    })
-    window.API.fetchPopularRepos(lang)
-      .then((data) => {
-        this.setState({
-          loading: false,
-          repos: data
-        })
-      })
-  }
 
-  //Handlers
-  handleRemoveRepo(name) {
-    this.setState((currentState) => {
-      return {
-        repos: currentState.repos.filter((repo) => repo.name !== name)
-      }
-    })
-  }
-  handleSelectLanguage(lang) {
-    this.setState({
-      activeLanguage: lang
-    })
-  }
-  
   render() {
     return (
       <Router>
@@ -98,6 +46,7 @@ class App extends Component {
               <Switch>
                 <Route exact path ='/' component={Home} />
                 <Route path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
                 <Route path='/popular' component={Popular} />
                 <Route render={function() {
                     return <p>Not Found</p>
---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+var React = require('react')
+var Link = require('react-router-dom').Link
+
+class Results extends React.Component {
+  render () {
+    return (
+      <div>
+        Results
+      </div>
+    )
+  }
+}
+
+module.exports = Results
